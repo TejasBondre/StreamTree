d99665cd644541db40369c12b41edac1fc10c6e5
Tejas Bondre <tejasbondre@gmail.com>
2015-02-02 11:18:58 -0500

    added basic data structure

---
+++ server.js
@@ -0,0 +1,18 @@
+
+/*
+   Basic datastructure for a server encapsulation
+*/
+
+var zerorpc = require('zerorpc');
+
+var Server = module.exports.Server = function (address) {
+};
+
+Server.prototype.getClient = function () {
+	// - - - 
+	// return client
+};
+
+Server.prototype.asSerializableObject = function() {
+	// return this
+};
