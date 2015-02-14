99985bd8081c23444e5566bca8ae42d928a239f9
Tejas Bondre <tejasbondre@gmail.com>
2015-02-13 23:46:42 -0500

    constructor for reporter object

--- reporter.js
+++ reporter.js
@@ -1,13 +1,20 @@
+'use strict';
+
 /*
   listener of chunk store and router of chunks
 */
 
-var Reporter = module.exports.Reporter = function (node, chunkStore, recipient) {
+
+var Reporter = module.exports.Reporter = function (node, chunkStore, recipient, fromWhom) {
   // chunkStore is a ChunkStore
   // recipient is a Server
   // fromWhome is a Server
-
-  // constructor
+  this.chunkStore = chunkStore;
+  this.recipient = recipient;
+  this.fromWhom = fromWhom;
+  this.node = node;
+  this.chunkStore.on('addedData', this.sendReport.bind(this, 'ADDED'));
+  this.chunkStore.on('deletedData', this.sendReport.bind(this, 'DELETED'));
 };
 
 Reporter.prototype.sendReport = function (action) {
