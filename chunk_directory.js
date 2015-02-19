dfe870fb96a54738574aad127998828606c85ba9
Tejas Bondre <tejasbondre@gmail.com>
2015-02-19 16:22:30 -0500

    require.js module for chunk directory

--- chunk_directory.js
+++ chunk_directory.js
@@ -34,3 +34,44 @@ ChunkDirectory.prototype.removeServer = function (server) {
   // remove entire server, all entries, everything
 };
 
+
+if (require.main === module) {
+  var Server = require('./server').Server;
+  
+
+  var cd = new ChunkDirectory();
+
+  cd.on('inserted', function (s) {
+    console.log('inserted', s.filename, s.chunk, 'on', s.server.name);
+  });
+
+  cd.on('deleted', function (s) {
+    console.log('deleted', s.filename, s.chunk, 'on', s.server.name);
+  });
+
+  cd.on('serverRemoved', function (s) {
+    console.log('removed server', s.name);
+  });
+
+  var s1 = new Server('inproc://foo1', 's1');
+  var s2 = new Server('inproc://foo2', 's2');
+
+  var f1 = 'file1';
+  var f2 = 'file2';
+  cd.insert(f1, 1, s1);
+  cd.insert(f1, 1, s2);
+  cd.insert(f2, 1, s1);
+  cd.insert(f1, 1, s1);
+  cd.insert(f1, 1, s1);
+  cd.insert(f1, 1, s1);
+  console.log('servers that have f1-1', cd.getServers(f1, 1));
+
+  cd.remove(f1, 1, s1);
+  cd.insert(f1, 1, s1);
+  console.log('servers that have f1-1', cd.getServers(f1, 1));
+  console.log('servers that have f2-1', cd.getServers(f2, 1));
+  cd.removeServer(s1);
+  console.log('servers that have f1-1', cd.getServers(f1, 1));
+  console.log('servers that have f2-1', cd.getServers(f2, 1));
+  
+}
